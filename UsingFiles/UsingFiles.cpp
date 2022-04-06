1d1905ebf4b66822ac36c16c7b081b9eb0186eea
Ryan Bieker <rbieke1@hotmail.com>
2022-04-05 23:28:18 -0600

    updates

--- UsingFiles/UsingFiles.cpp
+++ UsingFiles/UsingFiles.cpp
@@ -1,5 +1,5 @@
-// title
-
+// Ryan Bieker, CS 3060 001, Chapter 8 Programming Challenges, due 4/5/2022, 
+//Reads in a file of names, then sorts the names
 #include <iostream>
 #include <fstream>
 
@@ -13,6 +13,7 @@ int main()
     string text;
     //name file but use assignment one
     ifstream names("names.txt");
+    string namesArray[20];
     
     //number of words to read in
     cout << "Enter number of names to read: ";
@@ -24,10 +25,9 @@ int main()
     }
     else
     {
+        //only put names into names array if file can be opened
         if(names.is_open())
         {
-            string namesArray[20];
-
             for (int i = 0; i < num; i++)
             {
                 names >> namesArray[i];
@@ -40,7 +40,7 @@ int main()
         }
 
         //call selection sort for file
-        //selectionSort(
+        selectionSort(namesArray, num);
     }
 }
 
@@ -65,6 +65,9 @@ void selectionSort(string words[], int num)
         }
 
         //swap values
+        string temp = words[minIndex];
+        words[minIndex] = words[i];
+        words[i] = temp;
 
     }
 }
